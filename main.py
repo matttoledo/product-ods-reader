e1a1787eb2b4ad263b6b140ab4e980aa08bec92e
toledo <maatt.toledo@gmail.com>
2023-06-12 16:32:04 -0300

    adding business rules

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def read_files(fileName):
     dict = data.to_dict()
     keys = dict.keys()
     count = 0
-    a_file = open("tables/products.csv","w")
     for key in keys:
         values = dict.get(key)
         for x in values:
@@ -20,11 +19,7 @@ def read_files(fileName):
                 sheets = fileName.replace("janela_","").replace(".ods","").replace("box_","").replace("porta_","").replace("open","").upper()
 
                 if not sheets:
-                    sheets = "OPEN"
-                if sheets == "2F":
-                    sheets = "DUAS"
-                if sheets == "4F":
-                    sheets = "QUATRO"
+                    sheets = "1F"
 
                 sheets = sheets.replace("_","")
                 width = values[x][0:3].replace("x","")
@@ -32,6 +27,8 @@ def read_files(fileName):
                 type = fileName.replace("_4f.ods","").replace("_2f.ods","").replace(".ods","").replace("_open","").upper()
 
                 if sheets == "BASCULA":
+                    type = "JANELA"
+                    sheets = "1F"
                     height = width
 
                 if type == "PORTA" or type == "JANELA":
@@ -39,24 +36,20 @@ def read_files(fileName):
                     width = height
                     height = aux
 
-
-
-                if type == "BASCULA":
-                    altura = width
-
                 largura = width
                 altura = height
-
+                category = "VIDRO-TEMPERADO"
                 product = {
-                    "category": "VIDRO_TEMPERADO",
+                    "category": category,
                     "type": type,
                     "sheets": sheets,
-                    "width": float(largura),
+                    "width": largura,
                     "height": altura,
                     "color": color,
+                    "key": category+'-'+type+'-'+sheets+'-'+color+'-'+width+'-'+height
                 }
                 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-                reponse = requests.post('https://4111-177-148-216-242.sa.ngrok.io/verly-service/product', data=json.dumps(product), headers=headers)
+                reponse = requests.post('http://localhost:8000/verly-service/product', data=json.dumps(product), headers=headers)
                 print(product)
                 print("------------------------------")
                 print(reponse.status_code)
